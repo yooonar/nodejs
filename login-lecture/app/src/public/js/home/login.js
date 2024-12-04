2038ef50e7c1aedc5f29c185ec8a8525baf6c53e
yooonar <aryooon33@gmail.com>
2024-12-05 01:43:38 +0900

    프론트 로그인 기능 구현

--- login-lecture/app/src/public/js/home/login.js
+++ login-lecture/app/src/public/js/home/login.js
@@ -23,8 +23,15 @@ function login() {
     })
     // res.json()의 반환 값은 Promise(자바스크립트 비동기 처리에 사용되는 객체)이다.
     .then((res) => res.json()) // 응답 데이터 받아오기 Promise 타입
-    .then(console.log); // .then((res) => console.log(res)); 이거랑 같음 // res.json() 을 받아옴
-    
+    // .then(console.log); // .then((res) => console.log(res)); 이거랑 같음 // res.json() 을 받아옴
+    .then((res) => {
+        if(res.success) {
+            location.href = "/"; // 로그인 성공 시 메인 페이지로 이동
+        } else {
+            alert(res.msg);
+        }
+    })
+    .catch((err) => { // 예외 처리
+        console.error("로그인 중 에러 발생"); // console.error(new Error("로그인 중 에러 발생"));
+    });
 }
-
-// console.log(id);
