85148cac80630fd43f94a2bac336a4d2a845e98d
yooonar <aryooon33@gmail.com>
2024-12-04 22:13:39 +0900

    app.js express 기본 사용으로 정리

--- login-lecture/app.js
+++ login-lecture/app.js
@@ -1,3 +1,23 @@
+const express = require("express");
+const app = express();
+
+// 메인 페이지
+app.get("/", (req, res) => {
+    res.send("루트 페이지");
+});
+
+// 로그인 페이지
+app.get("/login", (req, res) => {
+    res.send("로그인 화면");
+});
+
+app.listen(3000, () => {
+    console.log("서버 가동");
+});
+
+
+/*
+// http 로 서버 가동.. 비추! express 사용하기!!
 const http = require ("http"); // http로 서버 띄우기
 const app = http.createServer((req, res) => {
     // console.log(req.url); // 루트 페이지의 경우 / 로 로그가 남는다. 라우팅 사용 가능
@@ -15,23 +35,5 @@ const app = http.createServer((req, res) => {
 
 app.listen(3001, () => {
     console.log("http로 서버 가동");
-});
-
-/*
-const express = require("express");
-const app = express();
-
-// 메인 페이지
-app.get("/", (req, res) => {
-    res.send("루트 페이지");
-});
-
-// 로그인 페이지
-app.get("/login", (req, res) => {
-    res.send("로그인 화면");
-});
-
-app.listen(3000, () => {
-    console.log("서버 가동");
 });
 */
