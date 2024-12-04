c08143c02ff003afb7341033e5bafca44c785de7
yooonar <aryooon33@gmail.com>
2024-12-04 22:04:16 +0900

    get 페이지 추가

--- login-lecture/app.js
+++ login-lecture/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+// 메인 페이지
+app.get("/", (req, res) => {
+    res.send("루트 페이지");
+});
+
+// 로그인 페이지
+app.get("/login", (req, res) => {
+    res.send("로그인 화면");
+});
+
 app.listen(3000, () => {
     console.log("서버 가동");
 });
